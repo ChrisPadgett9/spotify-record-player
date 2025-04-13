a9e6df1e9d9d5e74dd2a11dc76a2e59745ed87b8
ChrisPadgett9 <ChristopherPadgett09@gmail.com>
2025-04-13 17:44:56 -0600

    Logs

--- main.py
+++ main.py
@@ -71,10 +71,12 @@ def main():
         print('oh')
         if (open == True):
             for i in range(0, 181, 5):   #make servo rotate from 0 to 180 deg
+                print("open true: setting angle to: ", i)
                 setAngle(i)     # Write to servo
                 time.sleep(0.002)
         elif (open == False):
             for i in range(180, -1, -5): #make servo rotate from 180 to 0 deg
+                print("open false: setting angle to: ", i)
                 setAngle(i)
                 time.sleep(0.002)
         time.sleep(1)
